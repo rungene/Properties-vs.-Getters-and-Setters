8f761f18fdd550ae768a344305679e3fd6dbf82c
rungene <rungenesamuel@gmail.com>
2023-01-13 07:27:34 +0000

    Properties vs. Getters and Setters

---
+++ README.md
@@ -0,0 +1,3 @@
+# Properties vs. Getters and Setters
+
+[Properties vs. Getters and Setters](https://python-course.eu/oop/properties-vs-getters-and-setters.php).
---
+++ properties-getters.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+class P:
+    def __init__(self, x):
+        self.set_x(x)
+    def get_x(self):
+        return self.__x
+    def set_x(self,x):
+        if  x < 0:
+            self.__x = 0
+        elif x > 1000:
+            self.__x = 1000
+        else:
+            self.__x = x
+
+class P2:
+    def __init__(self, x):
+        self.x = x
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, x):
+        if x < 0:
+            self.__x = 0
+        elif x > 1000:
+            self.__x = 1000
+        else:
+            self.__x = x
+
+class P3:
+    def __init__(self, x):
+        self.set_x(x)
+    def get_x(self):
+        return self.__x
+    def set_x(self, x):
+        if x < 0:
+            self.__x = 0
+         elif x > 1000:
+            self.__x = 1000
+         else:
+            self.__x = x
+    x = property(get_x, set_x)
+
+class P4:
+    def __init__(self, x):
+        self.__set_x(x)
+    def __get_x(self):
+        return self.__x
+    def __set_x(self, x):
+        if x < 0:
+            self.__x = 0
+        elif x > 1000:
+            self.__x = 1000
+        else:
+            self.__x = x
+     x = property(__get_x, __set_x)
